f3c9ebe3786229a2910a31dc331491a197346f80
Gerry Gao <hologerry@gmail.com>
2021-01-05 23:10:21 +0800

    add pixpro_fcos_configs

--- configs/fcos/fcos_r50_caffe_fpn_mstrain_1x_coco.py
+++ configs/fcos/fcos_r50_caffe_fpn_mstrain_1x_coco.py
@@ -82,7 +82,7 @@ test_pipeline = [
         transforms=[
             dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Normalize', **timg_norm_cfg),
             dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
